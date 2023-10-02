f238fe5261fe4a33421d2e8c515edaad30ad229c
issmuth <ismael.mdev@gmail.com>
2023-10-02 21:03:14 +0300

    done

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly
+ * linked list has a cycle in it
+ * @list: list
+ *
+ * Return: o if there is no cycle 1 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
