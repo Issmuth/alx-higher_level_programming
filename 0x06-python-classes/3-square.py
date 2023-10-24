18810e4b860867df14d776a762e8e87a84d29408
issmuth <ismael.mdev@gmail.com>
2023-10-24 16:54:34 +0300

    done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# 3-square.py by Ismael
+""" Square Object Definition"""
+
+
+class Square:
+    """ a square object"""
+    def __init__(self, size=0):
+        """ initialize a square
+        Args: size - size of a square
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less that zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """ calculates the current square area
+        Returns: area of the current square
+        """
+
+        return (self.__size ** 2)
