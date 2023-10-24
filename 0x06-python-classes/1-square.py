c28e0cc281ca265d54db75488f3b412712237acc
issmuth <ismael.mdev@gmail.com>
2023-10-24 16:20:55 +0300

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 1-square.py by Ismael
+""" Square Definition"""
+
+
+class Square:
+    def __init__(self, size):
+        """ Initialize the square
+        Args: size - size of the square
+        """
+
+        self.size = size
