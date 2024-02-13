39d61b946d8cc3ff1e0698d040822c94d6773152
Issmuth <Ismael.mdev@gmail.com>
2024-02-13 18:40:04 +0300

    done

---
+++ 0x0D-SQL-introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- lists all rows of the table first_table in the database hbtn_0c_0 in MySQL server.
+
+SELECT * FROM first_table;
---
+++ 0x0D-SQL-introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- inserts a new row in the table first_table in MySQL server
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+
+-- The database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
