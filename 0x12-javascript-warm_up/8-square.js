0686939ffb076e6e3f5774be3ac3b18e116f8478
Issmuth <Ismael.mdev@gmail.com>
2024-03-12 17:04:30 +0300

    done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const num = Number(process.argv[2]);
+if (Number.isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row = row + 'X';
+    }
+    console.log(row);
+  }
+}
