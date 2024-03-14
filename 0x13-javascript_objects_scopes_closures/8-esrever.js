9cb5178882a48a2079d6e603f6cb3f8b92304fc3
Issmuth <Ismael.mdev@gmail.com>
2024-03-14 16:52:17 +0300

    done

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  newlist = [];
-  for (j = list.length - 1; j >= 0; j--) {
+  let newlist = [];
+  for (let j = list.length - 1; j >= 0; j--) {
     newlist.push(list[j]);
   }
   return newlist;
