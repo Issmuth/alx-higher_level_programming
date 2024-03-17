501ae8d4fbc3a1ceaafac682a7f92749be666aab
Issmuth <Ismael.mdev@gmail.com>
2024-03-18 00:27:14 +0300

    done

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that creates a State
+with a city along with it.
+"""
+from relationship_city import City
+from relationship_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(sys.argv[1], sys.argv[2],
+                               sys.argv[3]), pool_pre_ping=True)
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+state1 = State(name="California")
+city1 = City(name="San Francisco")
+
+state1.cities.append(city1)
+
+session.add(state1)
+session.add(city1)
+session.commit()
+session.close()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,7 @@ that inherits from declarative_base
 to map with corresponding table.
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
 
 
