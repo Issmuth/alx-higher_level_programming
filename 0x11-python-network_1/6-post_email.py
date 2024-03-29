6e68818a2128844c96202bc6e5627f7ff0a25a7d
Issmuth <Ismael.mdev@gmail.com>
2024-03-30 00:35:56 +0300

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,4 +6,4 @@ import sys
 if __name__ == "__main__":
     data = {'email': sys.argv[2]}
     response = request.post(sys.argv[1], data=data)
-    print(response.content)
+    print(response.read().decode('utf-8'))
