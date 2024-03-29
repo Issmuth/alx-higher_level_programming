61de3d7bfa1f4433226d71a8d265aa71eb556d5d
Issmuth <Ismael.mdev@gmail.com>
2024-03-30 01:05:33 +0300

    done?

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Takes a letter an sends a POST request to URL."""
+import requests
+import requests.exceptions
+import sys
+
+if __name__ == "__main__":
+    data = {'q': ""}
+    if len(sys.argv) > 1:
+        data['q'] = sys.argv[1]
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    try:
+        rjson = response.json()
+        if not rjson:
+            print('No result')
+        else:
+            print('[{}] {}'.format(rjson['id'], rjson['name']))
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
