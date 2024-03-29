98fecce10103ebe6d63f2ab3534994c41fdc1875
Issmuth <Ismael.mdev@gmail.com>
2024-03-30 01:39:46 +0300

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Displays GitHub users ID."""
 import requests
+from requests.auth import HTTPBasicAuth
 import sys
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/' + sys.argv[1]
-    header = {'Authorization': 'Bearer {}'.format(sys.argv[2])}
-    response = requests.get(url, headers=header)
-    print(response.json()['Id'])
+    authentication = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    response = requests.get('https://api.github.com/user', auth=authentication)
+    print(response.json().get('id'))
