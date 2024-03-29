2d0d7a78d8be9cb4e68ad44cd6247b2b5097c317
Issmuth <Ismael.mdev@gmail.com>
2024-03-30 00:05:27 +0300

    done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,8 +4,9 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 import sys
 
-try:
-    with urlopen(sys.argv[1]) as response:
-        print(response.read().decode('utf-8'))
-except HTTPError as e:
-    print('Error code: {}'.format(e.code))
+if __name__ == "__main__":
+    try:
+        with urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
