90481a61b55efcc4a5d6ff05692e5e17873b9501
Issmuth <Ismael.mdev@gmail.com>
2024-03-30 00:04:31 +0300

    done

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and displays the response."""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import sys
+
+try:
+    with urlopen(sys.argv[1]) as response:
+        print(response.read().decode('utf-8'))
+except HTTPError as e:
+    print('Error code: {}'.format(e.code))
