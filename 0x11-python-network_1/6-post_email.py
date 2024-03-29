2f7f8eaa3062efde9e2bd51a6b232ae84ff4093f
Issmuth <Ismael.mdev@gmail.com>
2024-03-30 00:31:37 +0300

    done

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Sends a POST request to URL with email."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    data = {'email': sys.argv[2]}
+    response = request.post(sys.argv[1], data=data)
+    print(response.text)
