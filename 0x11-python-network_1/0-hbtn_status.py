4abfdf235dc2d70ddb1d97cf5e4d8ffbf8ea19ca
Issmuth <Ismael.mdev@gmail.com>
2024-03-29 18:25:56 +0300

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@ from urllib.request import urlopen
 
 with urlopen('https://alx-intranet.hbtn.io/status') as s:
     response = {}
-    response['type'] = type(s)
-    response['content'] = s.read()
-    response['utf8 content'] = response['content'].decode('utf-8')
+    content = s.read()
+    response['type'] = type(content)
+    response['content'] = content
+    response['utf8 content'] = content.decode('utf-8')
     print('Body response:')
     for k, v in response.items():
         print("    - {}: {}".format(k, v))
